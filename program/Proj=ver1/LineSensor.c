4aa6a0e139b31b9d804efb1458785fef81e748c2
BartekOrchowski <bartek.orchowski@gmail.com>
2025-06-10 15:55:04 +0200

    Line sensors ADC

--- program/Proj=ver1/LineSensor.c
+++ program/Proj=ver1/LineSensor.c
@@ -1,20 +1,43 @@
 #include "LineSensor.h"
 
+// Próg dla wykrywania linii (dostosuj w zale¿noœci od czujnika i warunków oœwietleniowych)
+#define LINE_THRESHOLD 512 // Próg ADC (0-1023 dla 10-bitowego ADC)
 
 void LineSensor_Init(void)
 {
-	DDRC &= ~((1 << PC0) | (1 << PC1)); // Input on pin 0 and 1 of port C
-	//PORTC &= ~((1 << PC0) | (1 << PC1)); wlacz&/wylacz| pull up
+	// Konfiguracja pinów PC0 (ADC0) i PC1 (ADC1) jako wejœcia
+	DDRC &= ~((1 << PC0) | (1 << PC1)); // Piny PC0 i PC1 jako wejœcia
+	PORTC &= ~((1 << PC0) | (1 << PC1)); // Wy³¹cz pull-up (czujniki analogowe tego nie wymagaj¹)
+
+	// Konfiguracja ADC
+	ADMUX = (1 << REFS0); // Referencja AVCC (5V), wybór ADC0 jako domyœlny kana³
+	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // W³¹cz ADC, preskaler 128
 }
 
 uint8_t LineSensor_Read(void)
 {
-	if ((PINC & (1 << PC0)) || (PINC & (1 << PC1))) // If one of the line sensors outputs is high
+	uint16_t adc_value0, adc_value1;
+
+	// Odczyt z ADC0 (PC0)
+	ADMUX = (ADMUX & 0xF0) | (0x00); // Wybierz kana³ ADC0
+	ADCSRA |= (1 << ADSC); // Rozpocznij konwersjê
+	while (ADCSRA & (1 << ADSC)); // Czekaj na zakoñczenie konwersji
+	adc_value0 = ADC; // Zapisz wynik
+
+	// Odczyt z ADC1 (PC1)
+	ADMUX = (ADMUX & 0xF0) | (0x01); // Wybierz kana³ ADC1
+	ADCSRA |= (1 << ADSC); // Rozpocznij konwersjê
+	while (ADCSRA & (1 << ADSC)); // Czekaj na zakoñczenie konwersji
+	adc_value1 = ADC; // Zapisz wynik
+
+	// Logika wykrywania linii
+	// Zak³adamy, ¿e linia to niska wartoœæ ADC (np. ciemna powierzchnia), a brak linii to wysoka wartoœæ
+	if (adc_value0 < LINE_THRESHOLD || adc_value1 < LINE_THRESHOLD)
 	{
-		return NOLINE;
+		return LINE; // Linia wykryta pod przynajmniej jednym czujnikiem
 	}
 	else
 	{
-		return LINE;
+		return NOLINE; // Brak linii
 	}
-}
+}}
--- program/Proj=ver1/LineSensor.h
+++ program/Proj=ver1/LineSensor.h
@@ -3,9 +3,8 @@
 #include <stdint.h> // For uint8_t
 #include <avr/io.h> // For AVR macros
 
-#define LINE	1
-#define NOLINE	0
+#define LINE    1
+#define NOLINE  0
 
 void LineSensor_Init(void);
 uint8_t LineSensor_Read(void);
-
